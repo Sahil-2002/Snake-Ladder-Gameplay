ef9d2c24aaadb47206b4ac8fa9437952413e0f62
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-09 16:49:40 +0530

    [Sahil]Add: starting position

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Snake and Ladder gameplay ");
+        int startposition=0;
+        System.out.println("Start position is "+startposition);
+
+
+
+
     }
+
+
+
 }
