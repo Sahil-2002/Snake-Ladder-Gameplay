c710594decd801be1546ddd961e3b04a58954949
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-09 17:20:36 +0530

    [Sahil]Add: randomly checking snake&ladder

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,34 @@ public class Main {
     {
         System.out.println("Welcome to Snake and Ladder gameplay ");
         int startposition=0;
+        int position=0;
         System.out.println("Start position is "+startposition);
 
         Random random = new Random();
         int dieroll = random.nextInt(6)+1;
+String arr[]={"No play","Snake","Ladder"};
+
+while(position<100) {
+    int arrindex = random.nextInt(arr.length);
+    String condition=arr[arrindex];
+    switch (condition) {
+        case "No play":
+            position = position;
+            System.out.println(position);
+            break;
+        case "Snake":
+            position = position - dieroll;
+            System.out.println(position);
+            break;
+        case "Ladder":
+            position = position + dieroll;
+            System.out.println(position);
+            break;
+
+
+    }
+}
+
 
 
     }
