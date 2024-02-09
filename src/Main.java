86822ebadfd920cb181c7580b300854279f09225
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-09 17:43:35 +0530

    [Sahil]Add : player reaching position 100

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,45 @@ public class Main {
         System.out.println("Start position is "+startposition);
 
         Random random = new Random();
-        int dieroll = random.nextInt(6)+1;
-String arr[]={"No play","Snake","Ladder"};
 
+String arr[]={"No play","Snake","Ladder"};
+int count =0;
 while(position<100) {
+    int dieroll = random.nextInt(6)+1;
     int arrindex = random.nextInt(arr.length);
     String condition=arr[arrindex];
     switch (condition) {
         case "No play":
             position = position;
-            System.out.println(position);
+            if(position<0){
+                position=startposition;
+            }
+            System.out.println("At die roll "+dieroll+" At no play the position of player is  "+position);
+
             break;
         case "Snake":
             position = position - dieroll;
-            System.out.println(position);
+            if(position<0){
+                position=startposition;
+            }
+            System.out.println("At die roll "+dieroll+" At Snake the position of player is  "+position);
             break;
         case "Ladder":
+
             position = position + dieroll;
-            System.out.println(position);
+            if(position<0){
+                position=startposition;
+            }
+            System.out.println("At die roll "+dieroll+" At Ladder the position of player is  "+position);
             break;
 
 
     }
+
+    count++;
+
 }
+        System.out.println("count is "+count);
 
 
 
